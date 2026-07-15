8d8443dcadac9e5a8cd5cb1fec0edbf92bd01182
ytindo <yvan.guekeng@unissey.com>
2026-07-15 10:31:51 +0200

    update doc to remove polyfills need, link openapi spec

--- sdk-react-demo/src/index.tsx
+++ sdk-react-demo/src/index.tsx
@@ -1,13 +1,9 @@
-import 'core-js';
-import 'regenerator-runtime/runtime';
-import '@webcomponents/webcomponentsjs';
-import 'lit/polyfill-support.js';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
